27a5999ab54e1ddbcb1f78217d528bd7614e8b15
ynshah3 <yashesperance3@gmail.com>
2021-03-23 10:20:29 -0400

    added functionality

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    p = parseInt(document.getElementById("principal").value);
+    r = parseInt(document.getElementById("rate").value);
+    y = parseInt(document.getElementById("years").value);
+    document.getElementById("r1").innerHTML = p;
+    document.getElementById("r2").innerHTML = (r * 0.21) + "%";
+    document.getElementById("r4").innerHTML = 2020 + y;
+
+    result = (p * y * r * 0.21)/100
+
+    document.getElementById("r3").innerHTML = result;
+
+    document.getElementById("result").style.display = "inline-block";
+}
+
+function updateValue(value) {
+  document.getElementById("rate-value").innerHTML = value * 0.21 + "%";
 }
         
